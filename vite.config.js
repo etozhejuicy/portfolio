7e4b86156499448bbb2015e31aac2cf77b5b2e72
serj <zenitovic@gmail.com>
2024-09-05 17:34:10 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from "vite";
 export default defineConfig({
   base: "./",
   build: {
-    outDir: "dist",
     rollupOptions: {
       input: {
         index: "/index.html",
