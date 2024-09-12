489a09c3df30c1c59230bc7006ce827443f841d5
Kind Samurai <57537080+etozhejuicy@users.noreply.github.com>
2024-09-12 13:55:54 +0300

    update contacts

--- src/js/components/contactForm.js
+++ src/js/components/contactForm.js
@@ -13,15 +13,48 @@ window.addEventListener("load", (e) => {
             data[key] = value;
         }
 
-        console.log(data);
+        const token = '7155676478:AAEjVwCxt3AtnUZL9-3bqxrv4b1Xw2RI52M';
 
-        // fetch('https://example.com/submit', {
-        //     method: 'POST',
-        //     body: JSON.stringify(data),
-        //     headers: {
-        //         'Content-Type': 'application/json'
-        //     }
-        // });
+        const chatId = 'Black_Magic_Feedback_bot'; // Replace with the chat ID you want to send the message to
+
+        form.addEventListener('submit', (e) => {
+
+            e.preventDefault();
+
+            const formData = new FormData(form);
+
+            const message = `Form submitted with data: ${JSON.stringify(Object.fromEntries(formData))}`;
+
+            sendTelegramMessage(token, chatId, message);
+        });
+
+
+        function sendTelegramMessage(token, chatId, message) {
+
+            const xhr = new XMLHttpRequest();
+
+            xhr.open('POST', `https://api.telegram.org/bot${token}/sendMessage`, true);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+
+            xhr.onload = function () {
+                if (xhr.status === 200) {
+                    console.log(`Message sent: ${xhr.responseText}`);
+                } else {
+                    console.error(`Error sending message: ${xhr.statusText}`);
+                }
+            };
+
+            xhr.onerror = function () {
+                console.error(`Error sending message: ${xhr.statusText}`);
+            };
+
+            const data = JSON.stringify({
+                chat_id: chatId,
+                text: message
+            });
+
+            xhr.send(data);
+        }
     });
 
 });
--- src/js/pages/contacts.js
+++ src/js/pages/contacts.js
@@ -1,61 +1,82 @@
 // styles
 import "../../scss/pages/contacts.scss";
 
-import "../components/contactForm";
+// import "../components/contactForm";
 
-document.querySelector("#app").innerHTML = `
+class Contacts {
+  constructor() {
+    this.events();
+  }
+
+  events() {
+    this.init();
+  }
+
+  init() {
+
+    document.querySelector("#app").innerHTML = `
 <main class="content">
   <section class="section section-content section-hero">
     <div class="container">
-      <div class="row g-3 justify-content-center">
+      <div class="row g-5">
         <div class="col-12">
-          <h3 class="h4 text-center">
-            Свяжитесь со мной любым <br />
-            удобным способом:
-          </h3>
+          <div class="row g-3 justify-content-center">
+            <div class="col-12">
+              <h3 class="h4 text-center">
+                Свяжитесь со мной любым <br />
+                удобным способом:
+              </h3>
+            </div>
+            <div class="col-auto">
+              <a href="https://vk.com/kindsamurai_vk" target="_blank" class="btn btn-icon btn-white btn-colored cursor-effect fs-6">
+                <i class="cl-icon-vk"></i>
+              </a>
+            </div>
+            <div class="col-auto">
+              <a href="https://wa.me/79204079479" target="_blank" class="btn btn-icon btn-white btn-colored cursor-effect fs-6">
+                <i class="cl-icon-whatsapp"></i>
+              </a>
+            </div>
+            <div class="col-auto">
+              <a href="https://t.me/black_magic_101" target="_blank" class="btn btn-icon btn-white btn-colored cursor-effect fs-6">
+                <i class="cl-icon-telegram"></i>
+              </a>
+            </div>
+          </div>
         </div>
-        <div class="col-auto">
-          <a href="https://vk.com/kindsamurai_vk" target="_blank" class="btn btn-icon btn-black btn-colored btn-dimmed cursor-effect">
-            <i class="cl-icon-vk"></i>
-          </a>
-        </div>
-        <div class="col-auto">
-          <a href="https://wa.me/79204079479" target="_blank" class="btn btn-icon btn-black btn-colored btn-dimmed cursor-effect">
-            <i class="cl-icon-whatsapp"></i>
-          </a>
-        </div>
-        <div class="col-auto">
-          <a href="https://t.me/black_magic_101" target="_blank" class="btn btn-icon btn-black btn-colored btn-dimmed cursor-effect">
-            <i class="cl-icon-telegram"></i>
-          </a>
+        <div class="col-12 d-none">
+          <form id="contactForm" class="row g-3">
+            <div class="col-12 col-md-6">
+              <div class="form-label form-label-column form-relative">
+                <label for="name">Ваше имя</label>
+                <input name="name" type="text" class="form-control" rows="1" value="" id="name" placeholder="Иван Петров" required />
+              </div>
+            </div>
+            <div class="col-12 col-md-6">
+              <div class="form-label form-label-column form-relative">
+                <label for="email">Почта для связи</label>
+                <input name="email" type="text" class="form-control" rows="1" value="" id="email" placeholder="example@mail.ru" required />
+              </div>
+            </div>
+            <div class="col-12">
+              <div class="form-label form-label-column form-relative">
+                  <label for="message">Ваше сообщение</label>
+                  <textarea name="message" type="text" class="form-control" data-autosize="true" style="flex:unset;resize:none;" rows="4" value="" id="message" placeholder="Введите текст" required></textarea>
+              </div>
+            </div>
+            <div class="col-12 col-md-4 offset-md-8">
+              <button type="submit" class="btn btn-white btn-colored w-100">
+                Отправить
+              </button>
+            </div>
+          </form>
         </div>
       </div>
-      <form id="contactForm" class="row g-3 d-none">
-        <div class="col-12 col-md-6">
-          <div class="form-label form-label-column form-relative">
-            <label for="name">Ваше имя</label>
-            <input name="name" type="text" class="form-control" rows="1" value="" id="name" placeholder="Иван Петров" />
-          </div>
-        </div>
-        <div class="col-12 col-md-6">
-          <div class="form-label form-label-column form-relative">
-            <label for="email">Почта для связи</label>
-            <input name="email" type="text" class="form-control" rows="1" value="" id="email" placeholder="example@mail.ru" />
-          </div>
-        </div>
-        <div class="col-12">
-          <div class="form-label form-label-column form-relative">
-              <label for="message">Ваше сообщение</label>
-              <textarea name="message" type="text" class="form-control" data-autosize="true" style="flex:unset;resize:none;" rows="4" value="" id="message" placeholder="Введите текст"></textarea>
-          </div>
-        </div>
-        <div class="col-12 col-md-4 offset-md-8">
-          <button type="submit" class="btn btn-white btn-colored w-100">
-            Отправить
-          </button>
-        </div>
-      </form>
     </div>
   </section>
 </main>
-`;
+      `;
+  }
+}
+
+new Contacts();
