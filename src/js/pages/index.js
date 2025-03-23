e37cb8e0a969c005ef55fe9b027d274a08422787
Kind Samurai <57537080+etozhejuicy@users.noreply.github.com>
2025-03-23 11:10:29 +0300

    update card

--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -4,20 +4,20 @@ import routes from "../class/routes";
 import logo from "/logo.svg";
 
 class Main {
-    constructor() {
-        this.events();
-    }
+  constructor() {
+    this.events();
+  }
 
-    events() {
-        this.init();
-    }
+  events() {
+    this.init();
+  }
 
-    init() {
-        document.querySelector("#app").innerHTML = `
+  init() {
+    document.querySelector("#app").innerHTML = `
       <main class="content">
-        <section class="section section-content section-hero">
+        <section class="section section-content">
           <div class="container">
-            <div class="card bg-danger">
+            <div class="card card-hero bg-danger">
                 <h1 class="fw-700 text-black wow fadeIn" data-wow-duration="0.5s" data-wow-delay="0s" id="typing-text">
                     Cоздание
                     и&nbsp;продвижение
@@ -26,7 +26,7 @@ class Main {
                 <h2 class="fs-6 wow text-black fadeIn" data-wow-duration="0.5s" data-wow-delay="0.25s">
                     Делаю проекты с душой и&nbsp;любовью <i class="fa-solid fa-heart text-warning"></i>
                 </h2>
-                <div class="section-hero-actions">
+                <div class="card-hero-actions">
                   <a href="${routes.urls['works']}" class="btn btn-xl btn-icon btn-black btn-colored btn-voluminous w-100 w-xs-100 w-sm-100 w-md-fit wow fadeIn" data-wow-duration="0.5s" data-wow-delay="0.5s">
                     <i class="fa-solid fa-briefcase"></i>
                     <span class="btn-icon-text">Смотреть работы</span>
@@ -36,11 +36,11 @@ class Main {
                     <span class="btn-icon-text">Связаться со мной</span>
                   </a>
                 </div>
-                <div class="section-hero-logo">
+                <div class="card-hero-logo">
                                         <div class="big">
                                             <img data-src="` +
-            logo +
-            `" class="lazyload" />
+      logo +
+      `" class="lazyload" />
                                         </div>
                                     </div>
             </div>
@@ -49,7 +49,7 @@ class Main {
       </main>
     `;
 
-    }
+  }
 }
 
 new Main();
